5a7239fb2b94c09820883959d307ecbfa78bf332
Paul Betts <paul@paulbetts.org>
2011-09-30 13:21:13 -0700

    Add libgit2 Loading At Runtime
    Unfortunately, while Visual Studio has many solutions for ensuring that
    your managed dependent DLLs get shipped alongside your project, it's got
    absolutely nothing to ensure your native P/Invoke DLLs get shipped.
    
    The easiest, most foolproof method to ensure that a managed DLL has its
    native dependency regardless of how it is built or shipped is to embed
    it as a resource and extract it before use. This patch ensures that
    LibGit2Sharp will always have the corresponding version of git2.dll.

---
+++ LibGit2Sharp/Core/AssemblyExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace LibGit2Sharp.Core
+{
+    public static class AssemblyExtensions
+    {
+        public static string ExtractResourceToFile(this Assembly assembly, string resourceName, string outPath)
+        {
+            assembly = assembly ?? Assembly.GetExecutingAssembly();
+
+            var fi = new FileInfo(outPath);
+            if (fi.Exists)
+            {
+                return fi.FullName;
+            }
+
+            using (var stream = assembly.GetManifestResourceStream(resourceName))
+            using (var output = File.Create(outPath))
+            {
+                int bytesRead = 0;
+                byte[] buf = new byte[4096];
+                while ((bytesRead = stream.Read(buf, 0, buf.Length)) > 0)
+                {
+                    output.Write(buf, 0, bytesRead);
+                }
+            }
+
+            return outPath;
+        }
+    
+        public static string ExtractResourceToString(this Assembly assembly, string resourceName, Encoding encoding = null)
+        {
+            assembly = assembly ?? Assembly.GetExecutingAssembly();
+            encoding = encoding ?? Encoding.Default;
+
+            using (var sr = new StreamReader(assembly.GetManifestResourceStream(resourceName), encoding))
+            {
+                return sr.ReadToEnd();
+            }
+        }
+    }
+}
